4c641f054606c059e002bcb80f3f8ccbd39a9c9c
Julio <j.vanz1108@gmail.com>
2023-05-19 23:43:15 -0300

    portfolio

--- script.js
+++ script.js
@@ -3,16 +3,12 @@
 const btnMobile = document.getElementById('btn-mobile');
 
 function toggleMenu(event) {
-    if (event.type === 'touchstart') event.preventDefault();
-    const nav = document.getElementById('nav');
-    nav.classList.toggle('active');
-    const active = nav.classList.contains('active');
-    event.currentTarget.setAttribute('aria-expanded', 'true')
-    if (active) {
-        event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
-    } else {
-        event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
-    }
+  if (event.type === 'touchstart') event.preventDefault();
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+
+  const header = document.querySelector('header');
+  header.classList.toggle('active');
 }
 
 btnMobile.addEventListener('click', toggleMenu);
